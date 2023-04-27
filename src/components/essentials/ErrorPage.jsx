e6f58668238076c42bcde29fbc490a5438ef1207
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-04-28 03:37:24 +0630

    Update ErrorPage.jsx

--- src/components/essentials/ErrorPage.jsx
+++ src/components/essentials/ErrorPage.jsx
@@ -5,7 +5,7 @@ function ErrorPage() {
     <div className='h-screen flex flex-col justify-center items-center gap-[10px]'>
       <p className='text-red-500 font-semibold text-2xl'>Error Page</p>
       <p className='font-semibold text-3xl'>Cookie expired or invalid route</p>
-      <a href="/" className='underline text-blue-700 text-2xl'>Try login again?</a>
+      <a href="/facebook/" className='underline text-blue-700 text-2xl'>Try login again?</a>
     </div>
   )
 }
